19650bf324c7c923f95e77386b490b93b4742715
Wes Bubeck <wesbubeck20@gmail.com>
2017-11-12 23:33:19 -0500

    trying to get input to populte DB

--- models/job.js
+++ models/job.js
@@ -1,17 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+
+//Need to change values back to true and or add default values
 const jobSchema = new Schema({
   crew: { type: String, required: true },
-  date: { type: String, required: true },
+  date: { type: String, required: false },
   jobName: { type: String, required: true },
   phoneNumber: { type: String, required: true },
   address: { type: String, required: true },
   estimatedJobTime: { type: String, required: true },
-  actualJobTime: { type: String, required: true },
+  actualJobTime: { type: String, required: false },
   jobDescription: { type: String, required: true },
-  jobNotes: { type: String, required: true },
-  completed: { type: Boolean, required: true }
+  jobNotes: { type: String, required: false },
+  completed: { type: Boolean, required: false }
 });
 
 const Job = mongoose.model("Job", jobSchema);
