6756a9f0767e39027cddea346f0198a8d169dcb1
Wes Bubeck <wesbubeck20@gmail.com>
2017-11-12 21:27:33 -0500

    addded the auth0 code to our app

---
+++ client/src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: 'crewscheduler.auth0.com',
+  clientId: '1Ho4fjepnjqw186dAtShBvzboy5EWKt7',
+  callbackUrl: 'http://localhost:3000/callback'
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import { makeMainRoutes } from './routes';
 
+const routes = makeMainRoutes();
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  routes,
+  document.getElementById('root')
+);
