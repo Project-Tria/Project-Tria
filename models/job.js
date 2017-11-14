81a4047a1ab2f59995762782f9e91ecd6cfd486d
Wes Bubeck <wesbubeck20@gmail.com>
2017-11-13 20:46:44 -0500

    Changed the schema

--- models/job.js
+++ models/job.js
@@ -4,16 +4,16 @@ const Schema = mongoose.Schema;
 
 //Need to change values back to true and or add default values
 const jobSchema = new Schema({
-  crew: { type: String, required: true },
-  date: { type: String, required: false },
+  crew: { type: String, required: true, },
+  date: { type: Date, required: true, },
   jobName: { type: String, required: true },
   phoneNumber: { type: String, required: true },
   address: { type: String, required: true },
   estimatedJobTime: { type: String, required: true },
-  actualJobTime: { type: String, required: false },
+  actualJobTime: { type: String, default: "" },
   jobDescription: { type: String, required: true },
-  jobNotes: { type: String, required: false },
-  completed: { type: Boolean, required: false }
+  jobNotes: { type: String, default: "" },
+  completed: { type: Boolean, default: false }
 });
 
 const Job = mongoose.model("Job", jobSchema);
