b92101552d691a724ce11ca2fd2a439571f42271
Denis Molloy <denis.molloy@gmail.com>
2017-11-16 13:10:11 -0500

    MyJobs.js displaying placeholder accordian-collapse jobs

--- client/src/pages/Crew/CrewMain.js
+++ client/src/pages/Crew/CrewMain.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import MyJobs from "../../components/Jobs/MyJobs";
 //should crew main only display job for the current date?
 
 class CrewMain extends Component {
@@ -7,11 +7,12 @@ class CrewMain extends Component {
   render() {
     return (
       <div className="container text-center">
-        <h1>Welcome to Crew Home</h1>
+        <h1>Today's Jobs</h1>
         <br />
         <a href="/" className="btn btn-info">Back</a>
         <br />
-
+        <br />
+        <MyJobs />
       </div>
     );
   }
