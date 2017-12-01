ac1eb23c292e131efda46fdeef330e92b8135d32
Denis Molloy <denis.molloy@gmail.com>
2017-12-01 15:30:59 -0500

    attempting to add other modal

--- client/src/components/Create/CreateJob.js
+++ client/src/components/Create/CreateJob.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+
 import "./CreateJob.css";
 // import API from "../../utils/API";
 // import FormBtn from "../../components/Form";
 // import { Input, TextArea, FormBtn } from "../../components/Form";
 
 class CreateJob extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: false };
+  }
+
+  toggleModal = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   renderCrew = props => {
     return this.props.crews.map(crew => (
       <option key={"header" + crew._id}>{crew.crewNameDB}</option>
@@ -12,147 +24,286 @@ class CreateJob extends Component {
   };
 
   render() {
-    return <div className="container text-center border background-region">
+    return (
+      <div className="container text-center border background-region">
         <div className="form-group row">
-          <label htmlFor="date-input" className=" col-form-label col-3 text-right">
+          <label
+            htmlFor="date-input"
+            className=" col-form-label col-3 text-right"
+          >
             Job Date:
           </label>
-          <input className="col-6 form-control" type="date" id="date-input" name="jobDate" value={this.props.jobDate} onChange={event => {
+          <input
+            className="col-6 form-control"
+            type="date"
+            id="date-input"
+            name="jobDate"
+            value={this.props.jobDate}
+            onChange={event => {
               this.props.handleInputChange(event);
-            }} />
+            }}
+          />
         </div>
 
         <div className="form-group row" id="crew-space">
-          <label htmlFor="crew-dropdown" className=" col-form-label col-3 text-right">
+          <label
+            htmlFor="crew-dropdown"
+            className=" col-form-label col-3 text-right"
+          >
             Select Crew:
           </label>
           <div className="form-group col-4" id="select-crew">
-            <select className="form-control" id="crew-dropdown" name="crewName" value={this.props.crewName} onChange={event => {
+            <select
+              className="form-control"
+              id="crew-dropdown"
+              name="crewName"
+              value={this.props.crewName}
+              onChange={event => {
                 this.props.handleInputChange(event);
-              }}>
+              }}
+            >
               <option>Select a crew</option>
               {this.renderCrew()}
             </select>
           </div>
           <span>&nbsp;</span>
-          <button type="button" className="btn btn-success col-2" id="add-crew-btn" data-target="#add-crew-modal" data-toggle="modal">
+          <button
+            type="button"
+            className="btn btn-success col-2"
+            id="add-crew-btn"
+            data-target="#add-crew-modal"
+            data-toggle="modal"
+          >
             Add Crew
           </button>
         </div>
 
-        <div className="modal fade" id="add-crew-modal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+        <div
+          className="modal fade"
+          id="add-crew-modal"
+          tabIndex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalLongTitle"
+          aria-hidden="true"
+        >
           <div className="modal-dialog" role="document">
             <div className="modal-content">
               <div className="modal-header">
                 <h5 className="modal-title" id="exampleModalLongTitle">
                   Add a New Crew
                 </h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close" />
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                />
               </div>
               <div className="modal-body">
                 <div className="form-group row">
                   <label htmlFor="new-crew" className="col-form-label">
                     New Crew Name:{" "}
                   </label>
-                  <input className="col-6 form-control" type="text" placeholder="[Crew Type] - [Crew Lead]" id="new-crew" name="crewNameDB" value={this.props.crewNameDB} onChange={event => {
+                  <input
+                    className="col-6 form-control"
+                    type="text"
+                    placeholder="[Crew Type] - [Crew Lead]"
+                    id="new-crew"
+                    name="crewNameDB"
+                    value={this.props.crewNameDB}
+                    onChange={event => {
                       this.props.handleInputChange(event);
-                    }} />
+                    }}
+                  />
                 </div>
               </div>
               <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-dismiss="modal">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                >
                   Close
                 </button>
-                <button type="button" className="btn btn-success" onClick={event => {
+                <button
+                  type="button"
+                  className="btn btn-success"
+                  onClick={event => {
                     this.props.addCrew();
-                  }}>
+                  }}
+                >
                   Create Crew
                 </button>
               </div>
             </div>
           </div>
         </div>
-
+        
         <div className="form-group row">
           <label htmlFor="job-crew" className="col-form-label col-3 text-right">
             Job Crew:{" "}
           </label>
-          <input className="col-6 form-control" type="text" placeholder="Enter Crew Members" id="job-crew" name="crewMembers" value={this.props.crewMembers} onChange={event => {
+          <input
+            className="col-6 form-control"
+            type="text"
+            placeholder="Enter Crew Members"
+            id="job-crew"
+            name="crewMembers"
+            value={this.props.crewMembers}
+            onChange={event => {
               this.props.handleInputChange(event);
-            }} />
+            }}
+          />
         </div>
 
         <div className="form-group row">
           <label htmlFor="job-name" className="col-form-label col-3 text-right">
             Job Name:{" "}
           </label>
-          <input className="col-6 form-control" type="text" placeholder="[job type] - [customer-name]" id="job-name" name="jobName" value={this.props.jobName} onChange={event => {
+          <input
+            className="col-6 form-control"
+            type="text"
+            placeholder="[job type] - [customer-name]"
+            id="job-name"
+            name="jobName"
+            value={this.props.jobName}
+            onChange={event => {
               this.props.handleInputChange(event);
-            }} />
+            }}
+          />
         </div>
 
         <div className="form-group row">
-          <label htmlFor="cust-phone" className="col-form-label col-3 text-right">
+          <label
+            htmlFor="cust-phone"
+            className="col-form-label col-3 text-right"
+          >
             Telephone:{" "}
           </label>
-          <input className="col-6 form-control" type="tel" placeholder="123-456-7890" name="custPhone" id="cust-phone" value={this.props.custPhone} onChange={event => {
+          <input
+            className="col-6 form-control"
+            type="tel"
+            placeholder="123-456-7890"
+            name="custPhone"
+            id="cust-phone"
+            value={this.props.custPhone}
+            onChange={event => {
               this.props.handleInputChange(event);
-            }} />
+            }}
+          />
         </div>
 
         <div className="form-group row">
-          <label htmlFor="cust-address" className="col-form-label col-3 text-right">
+          <label
+            htmlFor="cust-address"
+            className="col-form-label col-3 text-right"
+          >
             Job Address:{" "}
           </label>
-          <input className="col-6 form-control" type="text" placeholder="Street" id="cust-address" name="custAddress" value={this.props.custAddress} onChange={event => {
+          <input
+            className="col-6 form-control"
+            type="text"
+            placeholder="Street"
+            id="cust-address"
+            name="custAddress"
+            value={this.props.custAddress}
+            onChange={event => {
               this.props.handleInputChange(event);
-            }} />
+            }}
+          />
         </div>
 
         <div className="form-group row">
-          <label htmlFor="cust-city" className="col-form-label col-3 text-right">
+          <label
+            htmlFor="cust-city"
+            className="col-form-label col-3 text-right"
+          >
             Job City:{" "}
           </label>
-          <input className="col-6 form-control" type="text" placeholder="City" id="cust-city" name="custCity" value={this.props.custCity} onChange={event => {
+          <input
+            className="col-6 form-control"
+            type="text"
+            placeholder="City"
+            id="cust-city"
+            name="custCity"
+            value={this.props.custCity}
+            onChange={event => {
               this.props.handleInputChange(event);
-            }} />
+            }}
+          />
         </div>
 
         <div className="form-group row">
-          <label htmlFor="cust-state" className="col-form-label col-3 text-right">
+          <label
+            htmlFor="cust-state"
+            className="col-form-label col-3 text-right"
+          >
             Job State:{" "}
           </label>
-          <input className="col-6 form-control" type="text" placeholder="State Abbreviation" id="cust-state" name="custState" value={this.props.custState} onChange={event => {
+          <input
+            className="col-6 form-control"
+            type="text"
+            placeholder="State Abbreviation"
+            id="cust-state"
+            name="custState"
+            value={this.props.custState}
+            onChange={event => {
               this.props.handleInputChange(event);
-            }} />
+            }}
+          />
         </div>
 
         <div className="form-group row">
-          <label htmlFor="job-descrip" className="col-form-label col-3 text-right">
+          <label
+            htmlFor="job-descrip"
+            className="col-form-label col-3 text-right"
+          >
             Job Notes:
           </label>
-          <textarea className="col-6 form-control" name="jobDescription" id="job-descrip" rows="3" value={this.props.jobDescription} onChange={event => {
+          <textarea
+            className="col-6 form-control"
+            name="jobDescription"
+            id="job-descrip"
+            rows="3"
+            value={this.props.jobDescription}
+            onChange={event => {
               this.props.handleInputChange(event);
-            }} />
+            }}
+          />
         </div>
 
         <div className="form-group row">
-          <label htmlFor="est-hours" className="col-form-label col-3 text-right">
+          <label
+            htmlFor="est-hours"
+            className="col-form-label col-3 text-right"
+          >
             Est. Hours
           </label>
-          <input className="col-6 form-control" type="number" name="estimatedJobTime" id="est-hours" value={this.props.estimatedJobTime} onChange={event => {
+          <input
+            className="col-6 form-control"
+            type="number"
+            name="estimatedJobTime"
+            id="est-hours"
+            value={this.props.estimatedJobTime}
+            onChange={event => {
               this.props.handleInputChange(event);
-            }} />
+            }}
+          />
         </div>
 
-        <button onClick={event => {
+        <button
+          onClick={event => {
             this.props.handleFormSubmit(event);
-          }} type="submit" className="btn btn-lg btn-primary">
+          }}
+          type="submit"
+          className="btn btn-lg btn-primary"
+        >
           Create Job
         </button>
 
         {/* <FormBtn onClick={this.handleFormSubmit}>Submit Job</FormBtn> */}
-      </div>; // end return
+      </div>
+    ); // end return
   } // end render
 } // end CreateJob
 
