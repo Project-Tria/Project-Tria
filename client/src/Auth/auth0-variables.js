9c7180b4f8546f95b855e7d3f7246d7f02525c7e
Denis Molloy <denis.molloy@gmail.com>
2017-12-02 13:21:59 -0500

    edited callbacck url

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'crewscheduler.auth0.com',
   clientId: '1Ho4fjepnjqw186dAtShBvzboy5EWKt7',
-  callbackUrl: 'https://whispering-tor-71112.herokuapp.com/'
+  callbackUrl: 'https://whispering-tor-71112.herokuapp.com/callback'
 }
